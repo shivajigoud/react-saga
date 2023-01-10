063fe16b0aed15555c483b5628868bfdea178132
shivajigoud <shiva.vumma@gmail.com>
2023-01-10 14:40:38 +0530

    1 file modified

--- src/restAPI/userAPI.js
+++ src/restAPI/userAPI.js
@@ -1,11 +1,39 @@
 import { axios } from 'axios';
 const userAPI = () => {
   const fetchAllUsers = () => {
-    return fetch('https://gorest.co.in/public/v2/users')
+    return fetch('https://gorest.co.in/public/v2/users1')
       .then((response) => ({ response }))
       .catch((error) => ({ error }));
   };
-  return { fetchAllUsers };
+  const createUser = (user) => {
+    return fetch(
+      'https://gorest.co.in/public/v2/users',
+      { method: post },
+      { data: JSON.stringify(user) }
+    )
+      .then((response) => ({ response }))
+      .catch((error) => ({ error }));
+  };
+  const updateUser = (id, user) => {
+    return fetch(
+      'https://gorest.co.in/public/v2/users/' + id,
+      { method: patch },
+      { data: JSON.stringify(user) }
+    )
+      .then((response) => ({ response }))
+      .catch((error) => ({ error }));
+  };
+  const deleteUser = (id) => {
+    return fetch('https://gorest.co.in/public/v2/users/' + id, { method: post })
+      .then((response) => ({ response }))
+      .catch((error) => ({ error }));
+  };
+  const fetchUser = (id) => {
+    return fetch('https://gorest.co.in/public/v2/users/' + id)
+      .then((response) => ({ response }))
+      .catch((error) => ({ error }));
+  };
+  return { fetchAllUsers, createUser, updateUser, deleteUser, fetchUser };
 };
 
 export default userAPI();
